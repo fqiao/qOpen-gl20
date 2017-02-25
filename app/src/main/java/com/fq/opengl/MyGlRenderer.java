cbcf4be0fe687858a22d2b274ac604b25071971f
fqiao <fqiao7234@sina.com>
2017-02-25 18:56:22 +0800

    add funtion:responding to Toch Events.

--- app/src/main/java/com/fq/opengl/MyGlRenderer.java
+++ app/src/main/java/com/fq/opengl/MyGlRenderer.java
@@ -3,6 +3,7 @@ package com.fq.opengl;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
 import com.fq.shapes.Square;
 import com.fq.shapes.Triangle;
@@ -23,6 +24,11 @@ public class MyGlRenderer implements GLSurfaceView.Renderer {
     private final float[] mProjectionMatrix = new float[16];
     private final float[] mViewMatrix = new float[16];
 
+    //Rotation matrix
+    private final float[] mRotationMatrix = new float[16];
+
+    private volatile float mAngle;
+
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -42,12 +48,30 @@ public class MyGlRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl10) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
+        float scrtch[] = new float[16];
+        /*
+        long time = SystemClock.uptimeMillis() % 4000L;
+        float angle = 0.090f * ((int) time);
+        */
+        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, -1.0f);
+
         //Set the camera position
         Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
 
         //Calculate the projection and view transformation
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
-        mTriangle.draw(mMVPMatrix);
+        Matrix.multiplyMM(scrtch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+
+        mTriangle.draw(scrtch);
+    }
+
+    public void setAngle(float angle) {
+        this.mAngle = angle;
+    }
+
+    public float getAngle() {
+        return this.mAngle;
     }
 }
--- app/src/main/java/com/fq/opengl/MyGlSurfaceView.java
+++ app/src/main/java/com/fq/opengl/MyGlSurfaceView.java
@@ -2,6 +2,7 @@ package com.fq.opengl;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
 
 /**
  * Created by acer on 2017/2/23.
@@ -9,14 +10,48 @@ import android.opengl.GLSurfaceView;
 
 public class MyGlSurfaceView extends GLSurfaceView {
 
+    private final float TOUCH_SCAL_FACTOR = 180.8F / 320;
+    private float previousX;
+    private float previousY;
+
     private final MyGlRenderer mRenderer;
 
-    public MyGlSurfaceView(Context context){
+    public MyGlSurfaceView(Context context) {
         super(context);
 
         setEGLContextClientVersion(2);
         mRenderer = new MyGlRenderer();
         setRenderer(mRenderer);
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                float dx = x - previousX;
+                float dy = y - previousY;
+
+                // reverse direction of rotation above the mid-line
+                if (y > getHeight() / 2) {
+                    dx *= -1;
+                }
+
+                // reverse direction of rotation to left of the mid-line
+                if (x > getWidth() / 2) {
+                    dy *= -1;
+                }
+
+                mRenderer.setAngle(mRenderer.getAngle() + ((dx + dy) * TOUCH_SCAL_FACTOR));
+                requestRender();
+        }
+
+        previousX = x;
+        previousY = y;
+        //return super.onTouchEvent(event);
+        return true;
     }
 }
